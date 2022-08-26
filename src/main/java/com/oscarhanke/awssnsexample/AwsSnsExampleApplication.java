69f7ec6b19328f809889884347b52d792dddcb5b
ohanke <steamoha@wp.pl>
2022-08-26 09:36:34 +0200

    Initial commit

---
+++ src/main/java/com/oscarhanke/awssnsexample/AwsSnsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.awssnsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSnsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSnsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarhanke/awssnsexample/AwsSnsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.awssnsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSnsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
